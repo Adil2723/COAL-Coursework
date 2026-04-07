e8990f8d7840f4eb0d48578fc988ba1ab72cfa78
Muhammad Adil Saeed <masadil942@gmail.com>
2026-04-07 23:40:22 +0500

    Add assembly program to calculate salary statistics

---
+++ Lab 4/task1.asm
@@ -0,0 +1,42 @@
+include irvine32.inc
+.data
+salaries dword 22000, 30000, 36000, 43000, 50000
+totalcount dword ?
+sumall dword ?
+avgsal dword ?
+msgtotal byte &quot;total number = &quot;,0
+msgsum byte &quot;Sum = &quot;,0
+msgavg byte &quot;Avg = &quot;,0
+.code
+main proc
+mov eax, lengthof salaries
+mov totalcount, eax
+mov eax, salaries[0*type salaries]
+add eax, salaries[1*type salaries]
+add eax, salaries[2*type salaries]
+add eax, salaries[3*type salaries]
+add eax, salaries[4*type salaries]
+mov sumall, eax
+mov ebx, totalcount
+xor edx, edx
+idiv ebx
+mov avgsal, eax
+mov edx, offset msgtotal
+call writestring
+mov eax, totalcount
+call writeint
+call crlf
+mov edx, offset msgsum
+call writestring
+mov eax, sumall
+call writeint
+call crlf
+mov edx, offset msgavg
+call writestring
+
+mov eax, avgsal
+call writeint
+call crlf
+exit
+main endp
+end main
