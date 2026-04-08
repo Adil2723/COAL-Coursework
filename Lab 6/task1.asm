066b2009d68b5d01d6a41e747e7a1e860a7e3be0
Muhammad Adil Saeed <masadil942@gmail.com>
2026-04-08 16:58:40 +0500

    Fix string encoding in task1.asm

--- Lab 6/task1.asm
+++ Lab 6/task1.asm
@@ -1,8 +1,8 @@
 INCLUDE Irvine32.inc
 .data
 numbers DWORD 21, 14, 9, 6, 18, 25, 30, 11, 12, 15
-msgResult BYTE &quot;Smallest number that is a multiple of 3: &quot;,0
-msgNone BYTE &quot;No number is a multiple of 3.&quot;,0
+msgResult BYTE "Smallest number that is a multiple of 3: ",0
+msgNone BYTE "No number is a multiple of 3.",0
 minNum DWORD 99999
 found DWORD 0
 .code
